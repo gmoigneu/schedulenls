d8ca22c0c5d33537a724c3db8035fd9da39f4e8d
nls <n@nls.io>
2017-12-03 10:09:16 +0100

    Add login:

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,3 +1,56 @@
-<form method="GET" action="/login">
-    <button>Login with Google</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('images/favicon.png') }}">
+    <title>{{ env('APP_NAME') }}</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="{{ url('plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <!-- page css -->
+    <link href="{{ url('css/style.css') }}" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="{{ url('css/style.css') }}" rel="stylesheet">
+    <link href="{{ url('css/blue.css') }}" id="theme" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body class="card-no-border">
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <section id="wrapper">
+        <div class="login-register">
+            <div class="login-box card">
+                <h1 class="text-center">scdl.me</h1>   
+                <h4 class="text-center">100% free personal meeting scheduler</h4>         
+                <div class="card-body">
+                    <form class="form-horizontal form-material" id="loginform" action="/login" method="GET">
+                        <div class="form-group text-center">
+                            <div class="col-xs-12 p-b-20">
+                                <button class="btn btn-block btn-lg btn-info btn-rounded" type="submit">Sign in with Google</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+</body>
+
+</html>
